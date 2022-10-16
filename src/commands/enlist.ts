cc58e643ce00b0d4b201fc85e7fadf75354a2078
xris nelson <chris.nelson@campspot.com>
2022-10-15 22:57:09 -0600

    fixy

--- src/commands/enlist.ts
+++ src/commands/enlist.ts
@@ -10,7 +10,7 @@ import { Command } from "../models/command";
 import charactersData from "../characters.json";
 import { DokeCharacter } from "../models/doke-character";
 import { CustomId } from "../models/custom-id";
-import { arrayChunkSplit } from "array-chunk-split";
+import { arrayChunkBySize } from "array-chunk-split";
 
 const CHARACTERS: DokeCharacter[] = charactersData as DokeCharacter[];
 
@@ -25,13 +25,15 @@ export const Enlist: Command = {
       }'`
     );
 
-    const charactersChunks = arrayChunkSplit(CHARACTERS, 3);
+    const charactersChunks = arrayChunkBySize(CHARACTERS, 18);
+
+    var count = 1;
 
     const components = charactersChunks.map((charactersChunk) => {
       return new ActionRowBuilder<SelectMenuBuilder>().addComponents(
         new SelectMenuBuilder()
           .setCustomId(`${CustomId.ENLIST_SELECT}_${Math.random() * 10000}`)
-          .setPlaceholder("choose your adventurer...")
+          .setPlaceholder(`choose your adventurer... (group ${count++})`)
           .addOptions(
             charactersChunk.map((character) => {
               return {
@@ -46,6 +48,7 @@ export const Enlist: Command = {
 
     await interaction.followUp({
       ephemeral: true,
+      content: "choose one adventurer from the following groups:",
       components,
     });
   },
