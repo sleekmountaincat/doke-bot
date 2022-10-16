a4629bc3db82664137808c4f5d7334ce65dee7c8
xris nelson <chris.nelson@campspot.com>
2022-10-16 14:42:30 -0600

    docker

---
+++ Dockerfile
@@ -0,0 +1,34 @@
+# transpile ts
+FROM node:16.9.1-alpine3.11 as ts-compiler
+
+WORKDIR doke-bot
+
+COPY src ./src
+
+COPY package.json .
+COPY package-lock.json .
+COPY tsconfig.json .
+
+RUN npm i
+
+RUN npm run build
+
+# grab node deps
+FROM node:16.9.1-alpine3.11 as node-builder
+
+WORKDIR doke-bot
+
+COPY package.json .
+COPY package-lock.json .
+COPY --from=ts-compiler /doke-bot/dist/ .
+
+RUN npm ci
+
+# distroless final stage
+FROM gcr.io/distroless/nodejs:16
+
+WORKDIR doke-bot
+
+COPY --from=node-builder doke-bot/ .
+
+CMD ["/doke-bot/doke-bot.js"]
--- src/common/helper-functions.ts
+++ src/common/helper-functions.ts
@@ -21,11 +21,11 @@ export async function removeExistingCharacterRolesFromMember(
 
   if (rolesToDelete.size) {
     Logger.info(
-      `${caller}: found existing roles, removing member '${member.displayName}' from: '${rolesToDelete.map(
-        (role) => {
-          return role.name;
-        }
-      )}'`
+      `${caller}: found existing roles, removing member '${
+        member.displayName
+      }' from: '${rolesToDelete.map((role) => {
+        return role.name;
+      })}'`
     );
 
     for (const role of rolesToDelete) {
@@ -46,18 +46,22 @@ export async function removeRosterEmbedsForMember(
   const rosterMessages = await rosterChannel.messages.fetch({ limit: 100 });
 
   for (const message of rosterMessages) {
-    const rosterEmbedPlayerField = message[1].embeds[0].fields.find(
-      (field) => field.name === "Player"
-    );
+    if (message[1].embeds && message[1].embeds.length) {
+      const rosterEmbedPlayerField = message[1].embeds[0].fields.find(
+        (field) => field.name === "Player"
+      );
 
-    if (rosterEmbedPlayerField?.value === `<@${user.id}>`) {
-      Logger.info(`${caller}: removing roster message for '${user.username}'`);
-
-      await rosterChannel.messages.delete(message[0]).catch((err) => {
-        Logger.warn(
-          `${caller}: could not remove roster message for '${user.username}': ${err}`
+      if (rosterEmbedPlayerField?.value === `<@${user.id}>`) {
+        Logger.info(
+          `${caller}: removing roster message for '${user.username}'`
         );
-      });
+
+        await rosterChannel.messages.delete(message[0]).catch((err) => {
+          Logger.warn(
+            `${caller}: could not remove roster message for '${user.username}': ${err}`
+          );
+        });
+      }
     }
   }
 }
